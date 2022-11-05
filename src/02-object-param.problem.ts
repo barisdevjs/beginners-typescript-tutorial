82e9dd1fe0ffdab41178a06572b88896cf6a395d
Barış SAVAŞ <barissavas17@gmail.com>
2022-11-05 20:02:00 +0300

    solved

--- src/02-object-param.problem.ts
+++ src/02-object-param.problem.ts
@@ -1,8 +1,33 @@
 import { expect, it } from "vitest";
 
-export const addTwoNumbers = (params) => {
+// solution 1
+
+type Params = {
+  first : number;
+  second : number;
+}
+
+/* export const addTwoNumbers = (params : Params) => {
   return params.first + params.second;
 };
+ */
+
+// solution 2 
+/* export const addTwoNumbers = (params : { first: number, second: number }) => {
+  return params.first + params.second;
+}; */
+
+// solution 3
+
+interface Params3 {
+  first : number,
+  second : number
+}
+export const addTwoNumbers = (params : Params3) => {
+  return params.first + params.second;
+};
+
+
 
 it("Should add the two numbers together", () => {
   expect(
